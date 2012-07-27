1b97f16fe363e04b4251caa751d612167b9d8b99
Pietro Pizzo <pietro.pizzo@gmail.com>
2012-07-27 14:59:15 +0200

    Initial checkin

---
+++ plot.py
@@ -0,0 +1,18 @@
+import mylib
+import db
+import sys
+import subprocess
+
+def plot_daily_details(date):
+    """Daily details plot"""
+
+    file = open("data.plot", mode="w")
+
+    result = db.read_daily_details(date)
+    for row in result:
+        for field in row[:-1]:
+            print(field, end="", file=file)
+            print("\t", end="", file=file)
+        print(row[-1:][0], file=file)
+
+#plot_daily_details("2012-07-27")
