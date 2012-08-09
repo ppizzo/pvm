8ec66b8ba416c857787772a388959701bc8c9531
Pietro Pizzo <pietro.pizzo@gmail.com>
2012-08-09 10:10:13 +0200

    Added configs

--- plot.py
+++ plot.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+#
+# Photovoltaic Monitor - Data plot module
+#
+# 2012, Pietro Pizzo <pietro.pizzo@gmail.com>
+######################################################################
+
 import mylib
 import db
 import sys
@@ -6,11 +13,15 @@ import subprocess
 def plot_daily_details(date):
     """Daily details plot"""
 
-    file = open("data.plot", mode="w")
+    try:
+        file = open(mylib.config_output_dir + "/" + mylib.config_plot_file, mode="w")
 
-    result = db.read_daily_details(date)
-    for row in result:
-        for field in row[:-1]:
-            print(field, end="", file=file)
-            print("\t", end="", file=file)
-        print(row[-1:][0], file=file)
+        result = db.read_daily_details(date)
+        for row in result:
+            for field in row[:-1]:
+                print(field, end="", file=file)
+                print("\t", end="", file=file)
+            print(row[-1:][0], file=file)
+
+    except Exception as e:
+        logging.error(e)
