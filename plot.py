7d5a2f57735313fe7a0526dd13d020de153f5ade
Pietro Pizzo <pietro.pizzo@gmail.com>
2012-08-09 17:32:59 +0200

    Minor changes

--- plot.py
+++ plot.py
@@ -10,18 +10,36 @@ import db
 import sys
 import subprocess
 
-def plot_daily_details(date):
-    """Daily details plot"""
-
-    try:
-        file = open(mylib.config_output_dir + "/" + mylib.config_plot_file, mode="w")
-
-        result = db.read_daily_details(date)
-        for row in result:
+def write_result(file, result):
+    """Write result to file suitable for gnuplot"""
+    for row in result:
             for field in row[:-1]:
                 print(field, end="", file=file)
                 print("\t", end="", file=file)
             print(row[-1:][0], file=file)
 
+def plot_daily_details(date):
+    """Daily details plot"""
+
+    try:
+        file = open(mylib.config_output_dir + "/" + mylib.config_daily_details_plot_file, mode="w")
+
+        result = db.read_daily_details(date)
+        write_result(file, result)
+
+        file.close()
+    except Exception as e:
+        logging.error(e)
+
+def plot_monthly_stats(date):
+    """Monthly stats plot"""
+
+    try:
+        file = open(mylib.config_output_dir + "/" + mylib.config_monthly_stats_plot_file, mode="w")
+
+        result = db.read_monthly_stats(date)
+        write_result(file, result)
+
+        file.close()
     except Exception as e:
         logging.error(e)
