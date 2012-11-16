17f1bf5e06642d9302022a1bd703fde0a58d6e1d
Pietro Pizzo <pietro.pizzo@gmail.com>
2012-11-16 11:48:18 +0100

    Added license disclaimer

--- create-db.sh
+++ create-db.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 #
-# This script creates the initial PVM database.
+# PVM PhotoVoltaic Monitor
 #
+# This script creates the initial PVM database.
 # Warning: all data will be erased!
 #
-# 2012, Pietro Pizzo <pietro.pizzo@hp.com>
+# Copyright (C) 2012 Pietro Pizzo <pietro.pizzo@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ########################################################################
 
 # Read configuration file
